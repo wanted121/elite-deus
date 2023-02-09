25221b0724161d167ee6d7f6d01baa74299fca50
Avinash Kumar Sharma <kumaravinashsharma82@gmail.com>
2023-02-09 08:00:13 +0530

    complete frontend and backend setup done

---
+++ frontend/elite_deus/src/components/ContactForms.vue
@@ -0,0 +1,94 @@
+<template>
+    <section>
+      
+            <form>
+
+                <div class="form-group">
+                    <label for="username">Name:</label>
+                    <input type="text" id="username" v-model="userdata.username">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" v-model="userdata.email">
+                </div>
+
+                <div class="form-group">
+                    <label for="massage">Massage:</label>
+                    <input type="text" id="massage" v-model="userdata.massage">
+                </div>
+                <button type="submit" @click='create'>Submit</button>
+
+            </form>
+        
+    
+    </section> 
+
+    </template>
+    
+    
+    <script>
+    import axios from 'axios'
+
+    export default {
+        data() {
+    return {  
+        userdata:{
+        username:'',
+        email:'',
+        massage:''
+        },
+
+      new_car:{
+          brand:'',
+          model:'',
+          engine:'',
+          gearbox:''
+        }
+        
+    } 
+        },
+    name: 'ContactForms',
+    mounted() {
+
+        axios
+            .get('http://localhost:4002/')
+            .then(response => (this.articles = response.data))
+            .catch(error => console.log(error))
+    },
+ 
+    methods: {
+       
+        create() {
+            
+            console.log("My data "+this.userdata.username);
+           //console.log(this.data121);
+//var parsedobj = jQuery.parseJSON( this.data121);
+  //          console.log(parsedobj);
+  
+  //console.log(data121);
+                axios
+                    .post('http://localhost:4002/',
+                        
+        this.userdata
+       // email: this.email,
+       // massage: this.massage
+     
+        
+    
+                        
+                    )
+                    .then(response => {
+                        console.log("res    "+response);
+                        return response;
+                    })
+                    .catch(error => console.log(error))
+        }
+    },
+
+    }
+    </script>
+    
+    <style>
+    
+    </style>
---
+++ frontend/elite_deus/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <ContactForms/>
+</template>
+
+<script>
+import ContactForms from '@/components/ContactForms.vue';
+export default {
+name: 'AboutView',
+components:{
+  ContactForms
+}
+}
+</script>
+
+<style>
+
+</style>
